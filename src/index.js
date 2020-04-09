c39c6b5f99f53594224a348f7df576ef8317383c
Dean Mitchell <dean.mitchell@freightways.co.nz>
2020-04-10 11:30:11 +1200

    tidied up unwanted code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-// var sm = require('./statemachine')
-// var pm = require('./asyncpromises')
-// var smasync = require('./statemachineasync')
-// var smc = require('./statemachinecollection')
 var factorydef = require('./EntModeller')
 
-// exports.StateMachine = sm
-// exports.StateMachineAsync = smasync
-// exports.StateMachineCollection = smc
 exports.EntModeller = factorydef
-// exports.Promises = pm
